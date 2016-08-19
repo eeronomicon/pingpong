e304267ae6558e41a97e4f03d7fade29e7d267b4
HK Kahng <hk@router-rooter.com>
2016-08-19 10:53:12 -0700

    add code to display output in 3 columns if longer than 8

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function pingSplit(userNumber) {
 function pingProcess(pingedArray) {
   for (i = 0; i < pingedArray.length; i++) {
     if (pingedArray[i] % 15 === 0) {
-        pingedArray[i] = "<span class=\"ping-pong\">ping-pong</span>";
+      pingedArray[i] = "<span class=\"ping-pong\">ping-pong</span>";
     } else if (pingedArray[i] % 5 === 0) {
       pingedArray[i] = "<span class=\"pong\">pong</span>";
     } else if (pingedArray[i] % 3 === 0) {
@@ -22,11 +22,26 @@ function pingProcess(pingedArray) {
 
 // Front End Code
 function pongDisplay(pongInput) {
-  $("#pongResults").empty();
+  $(".pongResults").empty();
   if (pongInput.length < 10) {
     pongInput.forEach(function(pongDigit) {
-      $("#pongResults").append("<li class=\"list-group-item\">" + pongDigit + "</li>");
+      $("#pongResults1").append("<li class=\"list-group-item\">" + pongDigit + "</li>");
     });
+  } else {
+    var pongOutputIndex = 0;
+    var noItemsPerCol = Math.ceil(pongInput.length / 3);
+    for (i = 1; i < 4; i++) {
+      var colName = "#pongResults" + i;
+      for (j = 0; j < noItemsPerCol; j++) {
+        if (pongOutputIndex < pingResult.length) {
+          pongAppend = "<li class=\"list-group-item\">" + pingResult[pongOutputIndex] + "</li>";
+        } else {
+          pongAppend = "";
+        }
+        $(colName).append(pongAppend);
+        pongOutputIndex++;
+      }
+    }
   }
 
 };
