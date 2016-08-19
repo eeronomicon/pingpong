6c07ab6be0d92e714feea68d4c68999154fac3e2
HK Kahng <hk@router-rooter.com>
2016-08-19 09:50:48 -0700

    add code to count from 1 to user input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 // Back End Code
+var pingArray = [];
+function pingSplit(userNumber) {
+  for (i = 1; i <= userNumber; i++) {
+    pingArray.push(i);
+  };
+  return pingArray;
+};
 
 
 // Front End Code
-$(document).ready (function(){
+function pongDisplay(pongInput) {
+  pongInput.forEach(function(pongDigit) {
+    $("#pongResults").append("<li>" + pongDigit + "</li>");
+  });
+};
 
+$(document).ready (function(){
+  $("#pingForm").submit(function(event) {
+    $("#pongResults").empty();
+    pingNumber = parseInt($("#pingInput").val());
+    pingResult = pingSplit(pingNumber);
+    pongDisplay(pingResult);
+
+    event.preventDefault();
+  });
 });
